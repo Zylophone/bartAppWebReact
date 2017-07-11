60ec73370bd82a6195bc27c34f947ee64f6386f2
nchnsn <nchnsn@gmail.com>
2017-07-11 11:58:15 -0700

    Tweaked the css and animations so arriving train doesnt overlap with times.

--- src/Body.js
+++ src/Body.js
@@ -13,9 +13,12 @@ const Body = (props) => {
     return (
       <div className="body default-text">
         <div className="App-body">
+          <div className="arrivingWrapper">
           <h1 className="trainArriving">{currentTrain === null ? '' : currentTrain.toUpperCase()}</h1>
-         <ul>{currentTrain !== null ? 'nothing' : props.trains.map((e,i)=><li key={i}><h3>{e[0]}</h3><p>{e[1]}, {e[2]}, {e[3]}</p></li>)}
-         </ul>
+          </div>{currentTrain === null ? <ul className="train-times">{ props.trains.map((e,i)=><li key={i}><h3>{e[0]}</h3><p>{e[1]}, {e[2]}, {e[3]}</p></li>)}{ props.trains.map((e,i)=><li key={i}><h3>{e[0]}</h3><p>{e[1]}, {e[2]}, {e[3]}</p></li>)}
+            </ul> : <ul className="train-times2"><p>{props.trains.map((e,i)=> e[0] + ' ' + e[1] + ' ' + e[2]  + ' ' + e[3] + ' ') }</p>
+            </ul> 
+          }
         </div>
       </div>
     );
